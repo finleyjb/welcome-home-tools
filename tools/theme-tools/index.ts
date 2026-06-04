85a84f3c37e53a735b7ff75d96b2f4b6669c3941
Finley Baker <finleyb.dev@proton.me>
2026-06-03 21:30:15 -0600

    Refine type exports

--- tools/theme-tools/index.ts
+++ tools/theme-tools/index.ts
@@ -1,7 +1,8 @@
-export * from './src/create-theme.ts';
+export { createTheme } from './src/create-theme.ts';
 export * from './src/fonts.ts';
 export {
   getCurrentTheme,
   setThemeName,
   subscribeCurrentTheme,
 } from './src/theme-store.ts';
+export type { Theme } from './src/types.ts';
