11b0f930b879b14b1f0734c02ab4cec6ea05a632
Finley Baker <finleyb.dev@proton.me>
2026-06-03 22:11:42 -0600

    Remove unused tsup config

--- api/theme-tools/tsup.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'tsup';
-
-export default defineConfig({
-  entry: ['index.ts'],
-  format: ['esm', 'cjs', 'iife'],
-  splitting: false,
-  sourcemap: true,
-  clean: true,
-});
