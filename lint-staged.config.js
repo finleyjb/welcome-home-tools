c2c1bd53ce7fe17ea5adc2d538b905ffcf7def82
Finley Baker <finleyb.dev@proton.me>
2026-06-04 12:09:19 -0600

    Update lint-staged to run biome

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,3 +1,4 @@
 export default {
-  '*.ts': (_stagedFiles) => ['pnpm test'],
+  '*.ts': (_stagedFiles) => ['pnpx @biomejs/biome check', 'pnpm test'],
+  '*.{css,ts,js,json}': (_stagedFiles) => ['pnpx @biomejs/biome check'],
 };
