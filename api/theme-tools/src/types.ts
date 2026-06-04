2f05a9568af44fd176f4e9f50b30349453c5b6fd
Finley Baker <finleyb.dev@proton.me>
2026-06-04 14:36:53 -0600

    Add link schema

--- api/theme-tools/src/types.ts
+++ api/theme-tools/src/types.ts
@@ -1,14 +1,30 @@
 import * as z from 'zod/mini';
 
-export const ThemeSchema = z.strictObject({
-  name: z.string(),
-  styleTitle: z.string(),
-  styleUrl: z.optional(z.httpUrl({ normalize: true })),
-  pageUrl: z.optional(z.array(z.httpUrl({ normalize: true }))),
-});
+export const ThemeSchema = z.readonly(
+  z.strictObject({
+    name: z.string(),
+    styleTitle: z.string(),
+    styleUrl: z.optional(z.httpUrl({ normalize: true })),
+    pageUrl: z.optional(z.array(z.httpUrl({ normalize: true }))),
+  }),
+);
 
 export type Theme = z.infer<typeof ThemeSchema>;
 
-export const ThemeDictSchema = z.strictObject({ themes: z.array(ThemeSchema) });
+export const LinkSchema = z.strictObject({
+  url: z.xor([z.string(), z.instanceof(RegExp)]),
+  defaultThemeName: z.string(),
+  preloadThemeNames: z.optional(z.array(z.string())),
+  preloadStrategy: z.optional(z.enum(['preload', 'prefetch'])),
+});
+
+export type Link = z.infer<typeof LinkSchema>;
+
+export const ThemeDictSchema = z.readonly(
+  z.strictObject({
+    themes: z.array(ThemeSchema),
+    urls: z.optional(z.array(LinkSchema)),
+  }),
+);
 
 export type ThemeDict = z.infer<typeof ThemeDictSchema>;
