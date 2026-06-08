2e5be47a9ffee22aa48793e14878a6883267aa09
Finley Baker <finleyb.dev@proton.me>
2026-06-08 17:11:38 -0600

    Remove commented-out code

--- api/theme-tools/src/types.ts
+++ api/theme-tools/src/types.ts
@@ -23,38 +23,9 @@ export const ThemeSchema = strictObject({
 
 export type Theme = InferOutput<typeof ThemeSchema>;
 
-// export const ThemeSchema = z.readonly(
-//   z.strictObject({
-//     name: z.string(),
-//     styleUrl: z.optional(z.httpUrl({ normalize: true })),
-//     pageUrl: z.optional(z.array(z.httpUrl({ normalize: true }))),
-//     preloadStrategy: z.optional(z.enum(['preload', 'prefetch'])),
-//   }),
-// );
-//
-// export type Theme = z.infer<typeof ThemeSchema>;
-
-// export const LinkSchema = z.strictObject({
-//   url: z.xor([z.string(), z.instanceof(RegExp)]),
-//   defaultThemeName: z.string(),
-//   preloadThemeNames: z.optional(z.array(z.string())),
-//   preloadStrategy: z.optional(z.enum(['preload', 'prefetch'])),
-// });
-//
-// export type Link = z.infer<typeof LinkSchema>;
-
 export const ThemeDictSchema = strictObject({
   themes: array(ThemeSchema),
 });
 
 export type ThemeDictInput = InferInput<typeof ThemeDictSchema>;
 export type ThemeDictOutput = InferOutput<typeof ThemeDictSchema>;
-
-// export const ThemeDictSchema = z.readonly(
-//   z.strictObject({
-//     themes: z.array(ThemeSchema),
-//     urls: z.optional(z.array(LinkSchema)),
-//   }),
-// );
-//
-// export type ThemeDict = z.infer<typeof ThemeDictSchema>;
