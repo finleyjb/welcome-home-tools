257399e0c6a5eea36a020c48dd17445d4008cd6a
Finley Baker <finleyb.dev@proton.me>
2026-06-09 15:44:16 -0600

    Run typecheck on commit

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,5 +1,9 @@
 export default {
-  '*.ts': (_stagedFiles) => ['pnpx @biomejs/biome check', 'pnpm test'],
+  '*.ts': (_stagedFiles) => [
+    'pnpx @biomejs/biome check',
+    'pnpm typecheck',
+    'pnpm test',
+  ],
   '*.{css,ts,js,json}': (_stagedFiles) => ['pnpx @biomejs/biome check'],
   '*.md': ['prettier --write'],
 };
